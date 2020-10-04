2dbb2ae1a0fa605af2353a0498a8c6f3eb2ad429
yuki.nakata <dorcushopeino1@gmail.com>
2020-10-04 17:47:04 +0900

    # セクション14:Firebase, Image Upload, Push Notifications - Building a Chat App - 308 ~ 317 cloud_firestoreを使ってFirebaseと接続 firebase読み込みと書き込みを行えるようにした

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class ChatScreen extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/Gf01OikouiwP4einMbjm/messages/')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: streamSnapshot.data.docs.length,
+            itemBuilder: (context, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton:
+          FloatingActionButton(child: Icon(Icons.add), onPressed: () {
+            FirebaseFirestore.instance
+            .collection('chats/Gf01OikouiwP4einMbjm/messages/')
+            .add({
+              'text': 'This was added by cliecing the button!'
+            });
+          }),
+    );
+  }
+}
